ed8cbd44f7577475e0977c6c7c0c406bced84f88
login <74485431+larkovsasha@users.noreply.github.com>
2022-08-01 21:38:20 +0300

    add: user

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModel } from './users.model';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [SequelizeModule.forFeature([UserModel])],
+})
+export class UsersModule {}
